094e4a73dda6ca4489f2bea11dc801b88b8fc7bf
Jamie Wilkinson <jamie@tramchase.com>
2008-04-01 16:03:14 +0000

    let's have the config files default be F.A.T. approved

--- config.sample.php
+++ config.sample.php
@@ -7,11 +7,11 @@
 #############################################################
 
 // name your fflickr install
-$NAME = "FuckFlickr Fotos!";
+$NAME = "F.A.T. PHOTOS";
 
 // URL to your fuckflickr system, with trailing slash
 // (optional, should autodetect just fine)
-$LINK = "http://example.com/fuckflickr/";
+$LINK = "http://fffff.at/fuckflickr/";
 
 // this is how many images fuckflickr resizes on one refresh
 // 10 is a good amnt - you dont want to hammer your server
@@ -27,7 +27,7 @@ $CLEAN_URLS = true;
 $CC_LICENSE = '<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/">Creative Commons Attribution-Noncommercial-Share Alike 3.0 License</a>';
 
 // your anti flickr message for the title & footer of each page
-$ANTI_FLICKR_MSG = "FUCK FLICKR!";
+$ANTI_FLICKR_MSG = "FUCK FLICKR";
 
 // separator for title
 $SEPARATOR = ' &ndash; ';
